6f21ac6220e16f9c2239522257a712f2578683a9
Vinicius Caetano <vinicaetano.f@gmail.com>
2025-04-22 00:04:37 -0300

    feat(ui): adiciona cabeçalho com título e botão de cadastro de alunos

--- front/src/views/students/index.vue
+++ front/src/views/students/index.vue
@@ -1,6 +1,19 @@
-<template>  
+<template>
+  <div>
+    <div class="d-flex justify-space-between align-center mb-4">
+      <h1 class="text-h4">Consulta de alunos</h1>
+      <v-btn
+        color="primary"
+        prepend-icon="mdi-plus"
+        aria-label="Cadastrar Aluno"
+        @click="$router.push({ name: 'students-create' })"
+      >
+        Cadastrar Alunos
+      </v-btn>
+    </div>
     <List></List>
+  </div>
 </template>
 <script setup>
-import List from './List.vue';
+import List from "./List.vue";
 </script>
