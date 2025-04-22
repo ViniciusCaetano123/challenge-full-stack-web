941b160d3b962bcad3f15ff42244aeed19b5536f
Vinicius Caetano <vinicaetano.f@gmail.com>
2025-04-21 23:59:41 -0300

    refactor: melhora estrutura da função createStudent

--- front/src/views/students/Create.vue
+++ front/src/views/students/Create.vue
@@ -1,20 +1,39 @@
+<!-- views/CreateStudent.vue -->
 <template>
-    <div>
-      <div class="d-flex justify-space-between align-center mb-4">
-        <h1 class="text-h4">Cadastro de Aluno</h1>
-        <v-btn
-          color="secondary"
-          prepend-icon="mdi-arrow-left"
-          @click="$router.push({ name: 'students' })"
-          aria-label="Voltar para lista de alunos"
-        >
-          Voltar
-        </v-btn>
-      </div>   
-      <StudentForm />
-    </div>
-  </template>
-  
-  <script setup>
-  import StudentForm from './Form.vue';
-  </script>
+  <StudentForm
+    title="Cadastro de Aluno"
+    submitText="Cadastrar"
+    cancelText="Limpar"
+    :loading="fetchCreateStudent?.loading"
+    :onSubmit="createStudent"
+    :onCancel="resetForm"
+    ref="studentForm"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import StudentForm from './Form.vue'
+import { useFetch } from '@/composables/useFetch'
+
+const loading = ref(false)
+const studentForm = ref(null)
+const fetchCreateStudent = ref(null)
+const createStudent = async (studentData) => {
+ 
+    loading.value = true
+    fetchCreateStudent.value =  useFetch('/students', {
+      method: 'POST',
+      data: studentData
+    })
+    await fetchCreateStudent.value?.fetch()
+    if (!fetchCreateStudent.value?.error) {
+      resetForm()
+    }
+ 
+}
+
+const resetForm = () => {
+  studentForm.value?.reset()
+}
+</script>
